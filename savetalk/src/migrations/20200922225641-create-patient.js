6c02743f0cf7c11e07a6ca0fe5a42a289fe9b50f
Rodo Mendoza Lucar <rodomendozalucar@MacBook-Pro.local>
2020-09-24 17:49:41 -0300

    creando development

---
+++ savetalk/src/migrations/20200922225641-create-patient.js
@@ -0,0 +1,49 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('patients', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+
+    age: {
+      type: Sequelize.INTEGER,
+    },
+    name: {
+      type: Sequelize.STRING,
+    },
+    phone: {
+      type: Sequelize.STRING,
+    },
+    address: {
+      type: Sequelize.STRING,
+    },
+    city: {
+      type: Sequelize.STRING,
+    },
+    picture: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      type: Sequelize.STRING,
+    },
+    rut: {
+      type: Sequelize.STRING,
+    },
+    isapre: {
+      type: Sequelize.STRING,
+    },
+
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+
+  down: (queryInterface) => queryInterface.dropTable('patients'),
+};
---
+++ savetalk/src/routes/patients.js
@@ -0,0 +1,24 @@
+const KoaRouter = require('koa-router');
+
+const router = new KoaRouter();
+
+router.param('id', async (id, ctx, next) => {
+    const patient = await ctx.orm.patient.findByPk(id);
+    if (!patient) {
+        ctx.throw(404);
+    }
+    ctx.state.patient = patient;
+    return next();
+});
+
+router.get('patients', '/', async (ctx) => {
+    const patients = await ctx.orm.patient.findAll();
+    await ctx.render('patients/index', {patients});
+});
+
+router.get('patient', '/:id', (ctx) => {
+    const {patient} = ctx.state;
+    return ctx.render('patients/show', {patient});
+});
+
+module.exports = router;
