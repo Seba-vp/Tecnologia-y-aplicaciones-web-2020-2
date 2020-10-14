8b39fe346a06beb6d73e1fa761a6ad2eb75ff502
Seba <svillablanca1@uc.cl>
2020-10-13 22:39:43 -0300

    trabajando en css

--- savetalk/src/routes/session.js
+++ savetalk/src/routes/session.js
@@ -1,9 +1,14 @@
 const KoaRouter = require('koa-router');
 
+const post = require('../models/post');
 const router = new KoaRouter();
 
+
 router.get('logging-menu', '/', async (ctx) => {
+    const posts = await ctx.orm.post.findAll();
     await ctx.render('session/index', {
+        posts,
+        postPath: id => ctx.router.url('post', id),
         patientRegistrationPath: ctx.router.url('patients-new'),
         dentistRegistrationPath: ctx.router.url('dentists-new'),
         loginPatientPath: ctx.router.url('session-create-patient'),
