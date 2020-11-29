bca9e227c6bd18a22f40832d7e6f89b150b5950c
Rodo Mendoza Lucar <rodomendozalucar@MacBook-Pro.local>
2020-11-28 22:45:40 -0300

    push external api

--- savetalk/src/routes/session.js
+++ savetalk/src/routes/session.js
@@ -1,10 +1,16 @@
 const KoaRouter = require('koa-router');
+const axios = require('axios');
 
 const post = require('../models/post');
 const router = new KoaRouter();
 
 router.get('logging-menu', '/', async (ctx) => {
     const posts = await ctx.orm.post.findAll();
+
+    var url = "http://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=497fa2c163be479dbc4fdb1a5e017cdc";
+    const res = await axios.get(url)
+    console.log("HOLLAA MIRAAR ACA: ", res.data.articles[0])
+
     await ctx.render('session/index', {
         posts,
         postPath: id => ctx.router.url('post', id),
